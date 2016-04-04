de0e6e445db4390d3f1326af6f3a9de2092158c9
Bruno Marco Visioli <bmvisioli@gmail.com>
2016-04-04 03:37:43 -0300

    Creates method to return runway types per country

--- src/main/java/com/github/bmvisioli/lun/output/Report.java
+++ src/main/java/com/github/bmvisioli/lun/output/Report.java
@@ -1,8 +1,48 @@
 package com.github.bmvisioli.lun.output;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import com.github.bmvisioli.lun.input.CountryData;
+import com.github.bmvisioli.lun.model.Country;
+
 public class Report {
 
 	public String build() {
 		return "reports";
 	}
+	
+	public String getTypesOfRunwayPerCountry() {
+		StringBuilder result = new StringBuilder();
+		List<Country> countryList = new CountryData().getCountryList();
+		countryList.forEach(country -> {
+			result.append(country.getName() + "\n");
+			List<String> runwayTypeList = new ArrayList<String>();
+			country.getAirportList().forEach(airport -> 
+				airport.getRunwayList().forEach(runway -> 
+					runwayTypeList.add(runway.getSurface())
+				)
+			);
+			runwayTypeList.stream().distinct().forEach(type -> result.append("|-" + type + "\n"));
+		});
+		return result.toString();
+	}
+
+	public String getCountriesByAirportCount(int size, boolean ascending) {
+		List<Country> countryList = new CountryData().getCountryList();
+		return countryList.stream()
+			.sorted((e1, e2) -> {
+				if(ascending) {
+					return Integer.compare(e1.getAirportList().size(),e2.getAirportList().size());
+				} else {
+					return Integer.compare(e2.getAirportList().size(),e1.getAirportList().size());
+				}
+			})
+			.limit(size)
+			.map(country -> country.getName() + " (" + country.getAirportList().size() + ")")
+			.collect(Collectors.joining(", "));
+	}
 }
+
--- src/test/java/com/github/bmvisioli/lun/output/ReportsTest.java
+++ src/test/java/com/github/bmvisioli/lun/output/ReportsTest.java
@@ -11,5 +11,17 @@ public class ReportsTest {
 	public void buildTest() {
 		assertEquals("reports", report.build());
 	}
+	
+	@Test
+	public void getTopCountriesByAirportQuantityTest() {
+		String expected = "United States (21501), Brazil (3839), Canada (2454), Australia (1908), Russia (920)";
+		assertEquals(expected, report.getCountriesByAirportCount(5, false));
+	}
+	
+	@Test
+	public void getTypesOfRunwayPerCountryTest() {
+		String expected = "Andorra\nUnited Arab Emirates\n|-ASP";
+		assertTrue(report.getTypesOfRunwayPerCountry().startsWith(expected));
+	}
 		
 }
